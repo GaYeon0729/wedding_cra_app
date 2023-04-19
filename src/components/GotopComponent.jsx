fbc9be7e115d599deaa05e8fe73c08e3be7521b5
GaYeon0729 <vkftjqdl8@naver.com>
2023-04-19 22:00:38 +0900

    Updates

--- src/components/GotopComponent.jsx
+++
@@ -1,7 +0,0 @@
-export default function GotopComponent(){
-    return(
-        <span className="go-top fade-in">
-            <a href="#wrap"><i class="fa fa-angle-up"></i></a>
-        </span>
-    )
-}
