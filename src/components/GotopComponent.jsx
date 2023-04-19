a19bbc562d2bce125032f1767a61974a1e7c047e
GaYeon0729 <vkftjqdl8@naver.com>
2023-04-19 21:50:49 +0900

    cra 웨딩 포트폴리오 배포

---
+++ src/components/GotopComponent.jsx
@@ -0,0 +1,7 @@
+export default function GotopComponent(){
+    return(
+        <span className="go-top fade-in">
+            <a href="#wrap"><i class="fa fa-angle-up"></i></a>
+        </span>
+    )
+}
